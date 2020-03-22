60d7bda223cb7e0f192623abbf9fd96baefdccd5
Mikolaj Kieres <Mikolaj Kieres>
2020-03-22 14:11:51 +1100

    Adding delete dialog when deleting player

--- board_games_companion/lib/pages/create_edit_player.dart
+++ board_games_companion/lib/pages/create_edit_player.dart
@@ -1,3 +1,4 @@
+import 'package:board_games_companion/common/dimensions.dart';
 import 'package:board_games_companion/common/styles.dart';
 import 'package:board_games_companion/models/player.dart';
 import 'package:board_games_companion/widgets/icon_and_text_button.dart';
@@ -19,14 +20,52 @@ class CreateEditPlayerPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(_isEditMode ? player.name : 'New Player'),
       ),
-      floatingActionButton: Opacity(
-        opacity: _isEditMode ? Styles.opaqueOpacity : Styles.transparentOpacity,
-        child: IconAndTextButton(
-          title: 'Delete ',
-          icon: Icons.delete,
-          color: Colors.redAccent,
-          onPressed: () {},
-        ),
+      body: Container(),
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          IconAndTextButton(
+            title: _isEditMode ? 'Update' : 'Create',
+            icon: Icons.create,
+            onPressed: () {},
+          ),
+          Divider(
+            indent: Dimensions.standardSpacing,
+          ),
+          Opacity(
+            opacity: true ? Styles.opaqueOpacity : Styles.transparentOpacity,
+            child: IconAndTextButton(
+              title: 'Delete',
+              icon: Icons.delete,
+              color: Colors.redAccent,
+              onPressed: () async {
+                await showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text(
+                          'Are you sure you want to delete ${player?.name}?'),
+                      elevation: 2,
+                      actions: <Widget>[
+                        FlatButton(
+                          child: Text('Cancel'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                        FlatButton(
+                          child: Text('Delete'),
+                          color: Colors.red,
+                          onPressed: () {},
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
