1af6f93c8bc78db07dd9ed670d6013b513b4b2d3
Mikolaj Kieres <Mikolaj Kieres>
2020-06-09 21:53:15 +1000

    Creating a banner for board game expansions to indicate that game is in the collection

--- board_games_companion/lib/widgets/board_games/details/board_game_details_expansions_widget.dart
+++ board_games_companion/lib/widgets/board_games/details/board_game_details_expansions_widget.dart
@@ -3,6 +3,7 @@ import 'package:board_games_companion/common/dimensions.dart';
 import 'package:board_games_companion/pages/board_game_details.dart';
 import 'package:board_games_companion/stores/board_game_details_store.dart';
 import 'package:board_games_companion/utilities/navigator_helper.dart';
+import 'package:board_games_companion/widgets/common/expansions_banner_widget.dart';
 import 'package:flutter/material.dart';
 
 import 'board_game_details_section_header_widget.dart';
@@ -35,33 +36,46 @@ class BoardGameDetailsExpansions extends StatelessWidget {
               _boardGameDetailsStore.boardGameDetails.expansions[index];
           return Material(
             color: Colors.transparent,
-            child: InkWell(
-              splashColor: AppTheme.accentColor,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: Dimensions.doubleStandardSpacing,
-                  horizontal: Dimensions.standardSpacing,
+            child: ClipRect(
+              child: CustomPaint(
+                foregroundPainter: ExpanionsBannerPainter(
+                  location: BannerLocation.topStart,
+                  color: AppTheme.accentColor,
+                  message: 'own',
                 ),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    Expanded(
-                      child: Text(
-                        expansion.name,
-                        style: AppTheme.theme.textTheme.headline3,
-                      ),
+                child: InkWell(
+                  splashColor: AppTheme.accentColor,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: Dimensions.doubleStandardSpacing,
+                      horizontal: Dimensions.standardSpacing,
                     ),
-                    Icon(
-                      Icons.navigate_next,
-                      color: AppTheme.accentColor,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        Expanded(
+                          child: Text(
+                            expansion.name,
+                            style: AppTheme.theme.textTheme.headline3,
+                          ),
+                        ),
+                        Icon(
+                          Icons.navigate_next,
+                          color: AppTheme.accentColor,
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
+                  onTap: () async {
+                    await NavigatorHelper.navigateToBoardGameDetails(
+                      context,
+                      expansion.id,
+                      expansion.name,
+                      BoardGamesDetailsPage,
+                    );
+                  },
                 ),
               ),
-              onTap: () async {
-                await NavigatorHelper.navigateToBoardGameDetails(context,
-                    expansion.id, expansion.name, BoardGamesDetailsPage);
-              },
             ),
           );
         })
