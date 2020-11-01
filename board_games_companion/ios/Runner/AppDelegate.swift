bce15259da907b73b3ee19947c249f5212ec42b8
Mikolaj Kieres <mikolaj@192-168-1-110.tpgi.com.au>
2020-11-01 14:39:25 +1100

    Updating iOS crashalitics to show up in the dashboard

--- board_games_companion/ios/Runner/AppDelegate.swift
+++ board_games_companion/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,8 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    FirebaseApp.configure()
+    GeneratedPluginRegistrant.register(with: self)    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
