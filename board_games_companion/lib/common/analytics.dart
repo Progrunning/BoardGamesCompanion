0f7c5f7f3542e15e31801b7ee6de18ef9c3366d7
Mikolaj Kieres <mikolaj.kieres@gmail.com>
2021-04-26 12:22:38 +1000

    Adding search phrase analtyics event

--- board_games_companion/lib/common/analytics.dart
+++ board_games_companion/lib/common/analytics.dart
@@ -4,6 +4,7 @@ class Analytics {
   static const String SortCollection = "sort_collection";
   static const String ViewGameStats = "view_game_stats";
   static const String ViewGameDetails = "view_game_details";
+  static const String SearchBoardGames = "search_board_games";
 
   static const String BoardGameIdParameter = "board_game_id";
   static const String BoardGameNameParameter = "board_game_name";
@@ -14,4 +15,6 @@ class Analytics {
 
   static const String FilterByParameter = "filter_by";
   static const String FilterByValueParameter = "filter_by_value";
+  
+  static const String SearchBoardGamesPhraseParameter = "phrase";
 }
