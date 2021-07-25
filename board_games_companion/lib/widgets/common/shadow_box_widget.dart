69e7a30568cd5df8c44602890ae9b4a02b57bf11
Mikolaj Kieres <mikolaj.kieres@gmail.com>
2021-07-25 10:55:58 +1000

    Fixing empty search screen

--- board_games_companion/lib/widgets/common/shadow_box_widget.dart
+++ board_games_companion/lib/widgets/common/shadow_box_widget.dart
@@ -1,22 +1,25 @@
-import 'package:board_games_companion/common/app_theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../../common/app_theme.dart';
 
 class ShadowBox extends StatelessWidget {
   const ShadowBox({
     @required this.child,
-  });
-  
+    Key key,
+  }) : super(key: key);
+
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(
-          boxShadow: [
-            AppTheme.defaultBoxShadow,
-          ],
-        ),
-        child: child);
+      decoration: const BoxDecoration(
+        boxShadow: [
+          AppTheme.defaultBoxShadow,
+        ],
+      ),
+      child: child,
+    );
   }
 }
