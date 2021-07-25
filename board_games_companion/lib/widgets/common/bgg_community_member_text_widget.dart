18a74e625291aac179ea7a2380f8bc7616156742
Mikolaj Kieres <mikolaj.kieres@gmail.com>
2021-07-25 12:01:20 +1000

    Updating the welcome screen

--- board_games_companion/lib/widgets/common/bgg_community_member_text_widget.dart
+++ board_games_companion/lib/widgets/common/bgg_community_member_text_widget.dart
@@ -1,18 +1,26 @@
-import 'package:board_games_companion/common/constants.dart';
-import 'package:board_games_companion/utilities/launcher_helper.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
+import '../../common/constants.dart';
+import '../../common/dimensions.dart';
+import '../../utilities/launcher_helper.dart';
+
 class BggCommunityMemberText extends StatelessWidget {
   const BggCommunityMemberText({
+    this.fontSize = Dimensions.mediumFontSize,
     Key key,
   }) : super(key: key);
+
+  final double fontSize;
 
   @override
   Widget build(BuildContext context) {
     return Text.rich(
       TextSpan(
         children: [
+          const TextSpan(
+            text: "If you're a member of the ",
+          ),
           const TextSpan(
             text: "If you're a member of the ",
           ),
@@ -42,6 +50,10 @@ class BggCommunityMemberText extends StatelessWidget {
             text: ' in the below box and hit the sync button to retrieve your collection.',
           ),
         ],
+      ),
+      textAlign: TextAlign.justify,
+      style: TextStyle(
+        fontSize: fontSize,
       ),
     );
   }
