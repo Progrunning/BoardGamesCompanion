538504c882c5efea320ace5c0ab8fafac5bfb57a
Mikolaj Kieres <mikolaj.kieres@gmail.com>
2021-08-22 11:25:18 +1000

    Removing table calendar and using date and time pickers

--- board_games_companion/lib/widgets/playthrough/calendar_card.dart
+++ board_games_companion/lib/widgets/playthrough/calendar_card.dart
@@ -4,15 +4,18 @@ import '../../common/app_theme.dart';
 import '../../common/dimensions.dart';
 import '../../common/styles.dart';
 import '../../extensions/date_time_extensions.dart';
+import '../common/rippler_effect.dart';
 import '../painters/divider_painter.dart';
 
 class CalendarCard extends StatelessWidget {
   const CalendarCard(
     this._date, {
+    this.onTap,
     Key key,
   }) : super(key: key);
 
   final DateTime _date;
+  final VoidCallback onTap;
   double get _width => 64;
 
   @override
@@ -20,7 +23,7 @@ class CalendarCard extends StatelessWidget {
     return Container(
       width: _width,
       decoration: const BoxDecoration(
-        color: AppTheme.defaultTextColor,
+        // color: AppTheme.white,
         borderRadius: BorderRadius.all(
           Radius.circular(Styles.boardGameTileImageCircularRadius),
         ),
@@ -28,58 +31,66 @@ class CalendarCard extends StatelessWidget {
           AppTheme.defaultBoxShadow,
         ],
       ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).accentColor,
-              borderRadius: const BorderRadius.only(
+      child: RippleEffect(
+        backgroundColor: AppTheme.white,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(Styles.boardGameTileImageCircularRadius),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Container(
+              decoration: const BoxDecoration(
+                color: AppTheme.accentColor,
+                borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(Styles.boardGameTileImageCircularRadius),
-                  topRight: Radius.circular(Styles.boardGameTileImageCircularRadius)),
-              boxShadow: const [BoxShadow(blurRadius: Styles.boardGameTileImageShadowBlur)],
-            ),
-            child: Center(
-              child: Text(
-                _date.toShortMonth('-'),
-                style: const TextStyle(
-                  color: AppTheme.defaultTextColor,
-                  fontWeight: FontWeight.bold,
+                  topRight: Radius.circular(Styles.boardGameTileImageCircularRadius),
+                ),
+                boxShadow: [BoxShadow(blurRadius: Styles.boardGameTileImageShadowBlur)],
+              ),
+              child: Center(
+                child: Text(
+                  _date.toShortMonth('-'),
+                  style: const TextStyle(
+                    color: AppTheme.defaultTextColor,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: Dimensions.halfStandardSpacing),
-            child: Center(
-              child: Text(
-                _date?.day?.toString() ?? '-',
-                style: const TextStyle(
-                    color: AppTheme.inverterTextColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: Dimensions.doubleExtraLargeFontSize),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: Dimensions.halfStandardSpacing),
+              child: Center(
+                child: Text(
+                  _date?.day?.toString() ?? '-',
+                  style: const TextStyle(
+                      color: AppTheme.inverterTextColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: Dimensions.doubleExtraLargeFontSize),
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              left: Dimensions.standardSpacing,
-              right: Dimensions.standardSpacing,
-              bottom: Dimensions.halfStandardSpacing,
+            Padding(
+              padding: const EdgeInsets.only(
+                left: Dimensions.standardSpacing,
+                right: Dimensions.standardSpacing,
+                bottom: Dimensions.halfStandardSpacing,
+              ),
+              child: CustomPaint(
+                size: const Size.fromHeight(1),
+                painter: DividerPainter(),
+              ),
             ),
-            child: CustomPaint(
-              size: const Size.fromHeight(1),
-              painter: DividerPainter(),
+            Text(
+              _date.toShortWeek('-'),
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color: AppTheme.inverterTextColor,
+              ),
             ),
-          ),
-          Text(
-            _date.toShortWeek('-'),
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              color: AppTheme.inverterTextColor,
-            ),
-          ),
-        ],
+          ],
+        ),
+        onTap: onTap,
       ),
     );
   }
