2cc3f1e718dcde333595c869d0a896232a29f9b2
Mikolaj Kieres <mikolaj.kieres@gmail.com>
2021-09-19 17:00:51 +1000

    Updating add player button

--- board_games_companion/lib/pages/home_page.dart
+++ board_games_companion/lib/pages/home_page.dart
@@ -2,21 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../common/app_theme.dart';
-import '../common/constants.dart';
 import '../extensions/page_controller_extensions.dart';
 import '../services/analytics_service.dart';
 import '../services/rate_and_review_service.dart';
 import '../stores/board_games_store.dart';
 import '../stores/home_store.dart';
 import '../stores/user_store.dart';
-import '../utilities/navigator_helper.dart';
 import '../widgets/common/bottom_tabs/custom_bottom_navigation_bar_item_widget.dart';
-import '../widgets/common/default_icon.dart';
-import '../widgets/common/icon_and_text_button.dart';
 import '../widgets/common/page_container_widget.dart';
 import 'base_page_state.dart';
 import 'games/games_page.dart';
-import 'players.dart';
+import 'players/players_page.dart';
 import 'search_board_games/search_board_games_page.dart';
 import 'settings/settings_page.dart';
 
@@ -41,13 +37,6 @@ class _HomePageState extends BasePageState<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final _addPlayerButton = IconAndTextButton(
-      icon: const DefaultIcon(
-        Icons.add,
-      ),
-      onPressed: () => _navigateToAddPlayerPage(context),
-    );
-
     return Scaffold(
       key: HomePage.homePageGlobalKey,
       body: PageContainer(
@@ -76,18 +65,7 @@ class _HomePageState extends BasePageState<HomePage> {
             homeStore.boardGamesPageIndex = pageIndex;
           },
         ),
-      ),
-      floatingActionButton: Consumer<HomeStore>(
-        builder: (_, homeStore, __) {
-          switch (homeStore.boardGamesPageIndex) {
-            case Constants.PlayersTabIndex:
-              return _addPlayerButton;
-            default:
-              return Container();
-          }
-        },
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      ),      
       bottomNavigationBar: Consumer<HomeStore>(
         builder: (_, homeStore, __) {
           return BottomNavigationBar(
@@ -107,9 +85,5 @@ class _HomePageState extends BasePageState<HomePage> {
         },
       ),
     );
-  }
-
-  Future<void> _navigateToAddPlayerPage(BuildContext context) async {
-    await NavigatorHelper.navigateToCreatePlayerPage(context);
-  }
+  }  
 }
