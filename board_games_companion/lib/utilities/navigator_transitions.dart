96eaea9cf9eaec6b1ad0a2a324b23b0f89e21811
Mikolaj Kieres <mikolaj.kieres@gmail.com>
2021-10-09 12:11:41 +1100

    Fixing imports

--- board_games_companion/lib/utilities/navigator_transitions.dart
+++ board_games_companion/lib/utilities/navigator_transitions.dart
@@ -1,6 +1,7 @@
 import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 
+// ignore: avoid_classes_with_only_static_members
 class NavigatorTransitions {
   static const int defaultDurationInMilliseconds = 1000;
 
