37cf93bd909a3629913f47f296bac380821dbd1c
Mikolaj Kieres <mkieres@users.noreply.github.com>
2022-01-24 22:05:55 +1100

    Updating chart and stats colors and text (#62)

--- board_games_companion/lib/common/app_text.dart
+++ board_games_companion/lib/common/app_text.dart
@@ -11,16 +11,23 @@ class AppText {
   static const Stop = 'Stop';
   static const Delete = 'Delete';
   static const Edit = 'Edit';
-  
+
   static const playthroughsStatisticsPageLastWinnerSectionTitle = 'Last winner';
   static const playthroughsStatisticsPageTopFiveSectionTitle = 'Top 5';
-  static const playthroughsStatisticsPagePlayerCountPercentageSectionTitle = 'Games % by player count';
-  static const playthroughsStatisticsPagePlayerWinsPercentageSectionTitle = 'Games % won by player';
-  static const playthroughsStatisticsPageQuickStatsSectionTitle = 'Quick Stats';
-  static const playthroughsStatisticsPageQuickStatsAvgPlaytime = 'Avg. playtime';
-  static const playthroughsStatisticsPageQuickStatsHighscore = 'Highscore';
-  static const playthroughsStatisticsPageQuickStatsAvgScore = 'Avg. score';
-  static const playthroughsStatisticsPageQuickStatsAvgPlayerCount = 'Avg. player count';
-  static const playthroughsStatisticsPageQuickStatsAvgPlayedGames = 'Played games';
+  static const playthroughsStatisticsPagePlayerCountPercentageSectionTitle =
+      'Games played by player count';
+  static const playthroughsStatisticsPagePlayerWinsPercentageSectionTitle =
+      'Games won by a player';
+  static const playthroughsStatisticsPageOverallStatsSectionTitle = 'Overall stats';
+  static const playthroughsStatisticsPageOverallStatsAvgPlaytime = 'Avg. playtime';
+  static const playthroughsStatisticsPageOverallStatsHighscore = 'Highscore';
+  static const playthroughsStatisticsPageOverallStatsAvgScore = 'Avg. score';
+  static const playthroughsStatisticsPageOverallStatsAvgPlayerCount = 'Avg. player count';
+  static const playthroughsStatisticsPageOverallStatsAvgPlayedGames = 'Played games';
   static const playthroughsStatisticsPagePlayersStatsSectionTitle = 'Players stats';
+  static const playthroughsStatisticsPagePlayersStatsPersonalBest = 'Personal best';
+  static const playthroughsStatisticsPagePlayersStatsAvgScore =
+      playthroughsStatisticsPageOverallStatsAvgScore;
+  static const playthroughsStatisticsPagePlayersStatsPlayedGames =
+      playthroughsStatisticsPageOverallStatsAvgPlayedGames;
 }
