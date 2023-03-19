3713f009c1870f85f96fff6caab4f7ee314aa5e7
mkieres <mikolaj.kieres@gmail.com>
2023-03-19 13:06:08 +1100

    sorting players on the edit playthrough by name

--- board_games_companion/lib/extensions/player_score_extensions.dart
+++ board_games_companion/lib/extensions/player_score_extensions.dart
@@ -1,3 +1,5 @@
+import 'package:board_games_companion/common/constants.dart';
+
 import '../common/enums/game_winning_condition.dart';
 import '../extensions/scores_extensions.dart';
 import '../models/player_score.dart';
@@ -7,6 +9,26 @@ extension PlayerScoresExtesions on List<PlayerScore> {
     return this
       ..sort((PlayerScore playerScore, PlayerScore otherPlayerScore) {
         return compareScores(playerScore.score, otherPlayerScore.score, winningCondition);
+      });
+  }
+
+  List<PlayerScore> sortByPlayerName() {
+    return this
+      ..sort((PlayerScore playerScore, PlayerScore otherPlayerScore) {
+        if (playerScore.player == null) {
+          if (otherPlayerScore.player != null) {
+            return Constants.moveBelow;
+          }
+
+          return Constants.leaveAsIs;
+        }
+
+        if (otherPlayerScore.player == null) {
+          return Constants.moveAbove;
+        }
+
+        return playerScore.player!.name?.compareTo(otherPlayerScore.player!.name ?? '') ??
+            Constants.moveBelow;
       });
   }
 }
