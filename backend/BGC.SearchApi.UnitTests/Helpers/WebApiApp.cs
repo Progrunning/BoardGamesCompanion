bfb1c2eb5250b5ab57cd1b6c5923a5a4a71f25bf
mkieres <mikolaj.kieres@gmail.com>
2023-06-11 18:30:42 +1000

    fixing unit tets

---
+++ backend/BGC.SearchApi.UnitTests/Helpers/WebApiApp.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace BGC.SearchApi.UnitTests.Helpers
+{
+    public sealed class WebApiApp : WebApplicationFactory<Program>
+    {
+        public const string ApiKey = "apiKey";
+        public const string MongoDbConnectionString = "mongoDbConnectionString";
+
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            builder.ConfigureHostConfiguration((configBuilder) =>
+            {
+                configBuilder.Sources.Clear();
+                configBuilder.AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    { "AppSettings:ApiKeyAuthenticationSettings:ApiKey", ApiKey },
+                    { "AppSettings:MongoDbSettings:ConnectionString", MongoDbConnectionString },
+                });
+            });
+
+            return base.CreateHost(builder);
+        }
+    }
+}
