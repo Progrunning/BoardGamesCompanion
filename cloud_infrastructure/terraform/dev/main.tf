23ee187eaa1d99a7f0cfc90e431fe6b4759daa15
mkieres <mikolaj.kieres@gmail.com>
2023-06-12 12:22:25 +1000

    configuring terraform backend in the main.tf file

--- cloud_infrastructure/terraform/dev/main.tf
+++ cloud_infrastructure/terraform/dev/main.tf
@@ -54,7 +54,15 @@ terraform {
       version = "~> 3.54.0"
     }
   }
+
+  # Can't use variables to populate this data
+  # See for details -> https://github.com/hashicorp/terraform/issues/13022
   backend "azurerm" {
+    subscription_id      = "2b318413-397e-40dc-b433-7aeee9e6f546"
+    resource_group_name  = "bgc-shared-rg"
+    storage_account_name = "bgcsharedst"
+    container_name       = "tfstate-dev"
+    key                  = "root.terraform.tfstate"
   }
 
   required_version = ">= 1.4.4"
