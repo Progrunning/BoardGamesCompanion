742541d08549d944ee39778951bd4125c5317e19
mkieres <mikolaj.kieres@gmail.com>
2023-06-17 17:14:30 +1000

    adding app insights to the deployment step

--- cloud_infrastructure/terraform/dev/main.tf
+++ cloud_infrastructure/terraform/dev/main.tf
@@ -117,11 +117,6 @@ resource "azurerm_container_app" "search_service_ca" {
       image  = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
       cpu    = 0.25
       memory = "0.5Gi"
-
-      env {
-        name        = "APPLICATIONINSIGHTS_CONNECTION_STRING"
-        secret_name = "appi-con-string"
-      }
     }
   }
 
