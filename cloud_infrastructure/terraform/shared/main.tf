f5e7485a2eb09f3da61b10c53de8d571aa955094
mkieres <mikolaj.kieres@gmail.com>
2023-06-18 13:32:40 +1000

    adding app insights for the apim

--- cloud_infrastructure/terraform/shared/main.tf
+++ cloud_infrastructure/terraform/shared/main.tf
@@ -16,6 +16,17 @@ variable "shared_resources" {
       name = string
       sku  = string
     })
+    analytics_workspace = object({
+      name              = string
+      retention_in_days = number
+      sku               = string
+    })
+    application_insights = object({
+      apim = object({
+        name          = string
+        insights_name = string
+      })
+    })
     apim = object({
       name = string
       sku  = string
@@ -73,20 +84,49 @@ resource "azurerm_storage_container" "storage_container_prod" {
 
 resource "azurerm_container_registry" "acr" {
   name                = var.shared_resources.container_registry.name
-  resource_group_name = var.shared_resources.resource_group.name
-  location            = var.shared_resources.resource_group.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   sku                 = var.shared_resources.container_registry.sku
 
   # MK Required for the deployment pipeline to push the image from shared ACR to container app
   admin_enabled = true
 }
 
+resource "azurerm_log_analytics_workspace" "log" {
+  name                = var.shared_resources.analytics_workspace.name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = var.shared_resources.analytics_workspace.sku
+  retention_in_days   = var.shared_resources.analytics_workspace.retention_in_days
+}
+
+resource "azurerm_application_insights" "apim_appi" {
+  name                = var.shared_resources.application_insights.apim.name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  workspace_id        = azurerm_log_analytics_workspace.log.id
+  application_type    = "other"
+}
+
 resource "azurerm_api_management" "apim" {
   name                = var.shared_resources.apim.name
-  resource_group_name = var.shared_resources.resource_group.name
-  location            = var.shared_resources.resource_group.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   publisher_name      = "Progrunning"
   publisher_email     = "info@progrunning.net"
 
   sku_name = var.shared_resources.apim.sku
 }
+
+resource "azurerm_api_management_logger" "apim-insights" {
+  name                = var.shared_resources.application_insights.apim.insights_name
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = azurerm_resource_group.rg.name
+  resource_id         = azurerm_application_insights.apim_appi.id
+
+  application_insights {
+    instrumentation_key = azurerm_application_insights.apim_appi.instrumentation_key
+  }
+}
+
+
