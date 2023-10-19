d4e03def4965a307ad8e1c09d06097cf59e506a1
mkieres <mikolaj.kieres@gmail.com>
2023-10-19 20:20:03 +1100

    disabling AzureWebJobsDashboard

--- cloud_infrastructure/terraform/prod/main.tf
+++ cloud_infrastructure/terraform/prod/main.tf
@@ -234,6 +234,7 @@ resource "azurerm_linux_function_app" "func" {
   storage_account_name       = azurerm_storage_account.sa.name
   storage_account_access_key = azurerm_storage_account.sa.primary_access_key
   service_plan_id            = azurerm_service_plan.asp.id
+  builtin_logging_enabled    = false
 
   site_config {
     application_stack {
