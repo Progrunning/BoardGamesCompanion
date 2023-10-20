5e9f19c7a1a8d42db84b4f9f231b9c3171402dff
mkieres <mikolaj.kieres@gmail.com>
2023-10-20 22:00:36 +1100

    excluding date time service from coverage

--- backend/BGC.Core/Services/DateTimeService.cs
+++ backend/BGC.Core/Services/DateTimeService.cs
@@ -1,7 +1,10 @@
-﻿using BGC.Core.Services.Interfaces;
+﻿using System.Diagnostics.CodeAnalysis;
+
+using BGC.Core.Services.Interfaces;
 
 namespace BGC.Core.Services
 {
+    [ExcludeFromCodeCoverage(Justification = "There's not really much to test here")]
     public class DateTimeService : IDateTimeService
     {
         public DateTimeOffset UtcOffsetNow => DateTimeOffset.UtcNow;
