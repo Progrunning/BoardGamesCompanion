7abce9d80211c0b3c3c2036d9d9960fc90f9237d
mkieres <mikolaj.kieres@gmail.com>
2023-10-20 22:16:48 +1100

    adding test to bump up the coverage

---
+++ backend/tests/BGC.Core.UnitTests/Extensions/RegionDtoExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using BGC.Core.Extensions;
+using BGC.Core.Models.Dtos.BoardGameOracle;
+
+namespace BGC.Core.UnitTests.Extensions
+{
+    public class RegionDtoExtensionsTests
+    {
+        [InlineData(RegionDto.Australia, "au")]
+        [InlineData(RegionDto.Canada, "ca")]
+        [InlineData(RegionDto.UnitedStates, "us")]
+        [InlineData(RegionDto.UnitedKingdom, "uk")]
+        [InlineData(RegionDto.NewZealand, "nz")]
+        public void ToAbbreviation_RegionDto_ConvertsToExpectedValue(RegionDto regionDto, string expectedAbbreviation)
+        {
+            var abbreviation = regionDto.ToAbbreviation();
+
+            abbreviation.Should().Be(expectedAbbreviation);
+        }
+    }
+}
