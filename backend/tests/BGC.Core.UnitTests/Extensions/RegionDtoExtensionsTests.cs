a2453090e8ed47771c3ec6a14e29fc878b5e2d12
mkieres <mikolaj.kieres@gmail.com>
2023-10-20 22:23:11 +1100

    adding missed attribution

--- backend/tests/BGC.Core.UnitTests/Extensions/RegionDtoExtensionsTests.cs
+++ backend/tests/BGC.Core.UnitTests/Extensions/RegionDtoExtensionsTests.cs
@@ -5,10 +5,11 @@ namespace BGC.Core.UnitTests.Extensions
 {
     public class RegionDtoExtensionsTests
     {
+        [Theory]
         [InlineData(RegionDto.Australia, "au")]
         [InlineData(RegionDto.Canada, "ca")]
         [InlineData(RegionDto.UnitedStates, "us")]
-        [InlineData(RegionDto.UnitedKingdom, "uk")]
+        [InlineData(RegionDto.UnitedKingdom, "gb")]
         [InlineData(RegionDto.NewZealand, "nz")]
         public void ToAbbreviation_RegionDto_ConvertsToExpectedValue(RegionDto regionDto, string expectedAbbreviation)
         {
