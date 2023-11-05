0fb433b8cfd92e8d7d825657fd7cb82977dbb67a
Mikolaj Kieres <mikolaj.kieres@gmail.com>
2023-11-05 12:10:12 +1100

    fixing score bugs and adding unit tests

---
+++ board_games_companion/test/mocks/game_playthroughs_details_store_mock.dart
@@ -0,0 +1,4 @@
+import 'package:board_games_companion/stores/game_playthroughs_details_store.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockGamePlaythroughsDetailsStore extends Mock implements GamePlaythroughsDetailsStore {}
--- board_games_companion/test/models/playthrough_details_test.dart
+++ board_games_companion/test/models/playthrough_details_test.dart
@@ -1,6 +1,7 @@
 import 'package:board_games_companion/models/hive/player.dart';
 import 'package:board_games_companion/models/hive/playthrough.dart';
 import 'package:board_games_companion/models/hive/score.dart';
+import 'package:board_games_companion/models/hive/score_game_results.dart';
 import 'package:board_games_companion/models/player_score.dart';
 import 'package:board_games_companion/models/playthroughs/playthrough_details.dart';
 import 'package:clock/clock.dart';
@@ -28,17 +29,57 @@ void main() {
 
   test(
       'GIVEN playthrough details  '
-      'WHEN there are scores with the same score '
+      'WHEN there are scores with tiebreakrer type defined '
       'THEN then the hasTies property should be true ', () {
     playthroughDetails = PlaythroughDetails(
       playthrough: emptyPlaythrough,
       playerScores: <PlayerScore>[
-        emptyPlayerScore.copyWith(score: emptyScore.copyWith(value: '100')),
-        emptyPlayerScore.copyWith(score: emptyScore.copyWith(value: '100')),
+        emptyPlayerScore.copyWith(
+          score: emptyScore.copyWith(
+            scoreGameResult: const ScoreGameResult(
+              points: 10,
+              place: 1,
+              tiebreakerType: ScoreTiebreakerType.place,
+            ),
+          ),
+        ),
+        emptyPlayerScore.copyWith(
+          score: emptyScore.copyWith(
+            scoreGameResult: const ScoreGameResult(
+              points: 10,
+              place: 1,
+              tiebreakerType: ScoreTiebreakerType.place,
+            ),
+          ),
+        ),
         emptyPlayerScore.copyWith(score: emptyScore.copyWith(value: '200')),
       ],
     );
 
     expect(playthroughDetails.hasTies, isTrue);
+  });
+
+  test(
+      'GIVEN playthrough details  '
+      'WHEN all scores have been enetered '
+      'THEN finished scoring flag should be set to true ', () {
+    playthroughDetails = PlaythroughDetails(
+      playthrough: emptyPlaythrough,
+      playerScores: <PlayerScore>[
+        emptyPlayerScore.copyWith(
+          score: emptyScore.copyWith(
+            scoreGameResult: const ScoreGameResult(points: 10),
+          ),
+        ),
+        emptyPlayerScore.copyWith(
+          score: emptyScore.copyWith(
+            scoreGameResult: const ScoreGameResult(points: 1),
+          ),
+        ),
+        emptyPlayerScore.copyWith(score: emptyScore.copyWith(value: '200')),
+      ],
+    );
+
+    expect(playthroughDetails.finishedScoring, isTrue);
   });
 }
